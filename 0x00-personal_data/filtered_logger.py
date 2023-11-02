9d23abac1bfa768b932533e4c609dc67d5951451
Fatimah1403 <fattyhassan14@gmail.com>
2023-11-02 21:42:35 +0000

    file updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,3 +24,27 @@ def filter_datum(fields: List[str], redaction: str,
         log_line = re.sub(field + "=.*?" + separator,
                           field + "=" + redaction + separator, log_line)
     return log_line
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """initializing"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.feilds = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+         filter values in incoming log records using filter_datum
+        """
+        filter_value = filter_datum(
+                self.fields, self.READACTION, super(
+                    RedactingFormatter, self).format(record),
+                self.SEPARATOR)
+        return filter_value
