0d94823af0ba09a10a122a4729593b283d4a0f07
Fatimah1403 <fattyhassan14@gmail.com>
2023-11-02 22:41:22 +0000

    New update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,22 +28,19 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """initializing"""
+        """ inits class instance """
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.feilds = fields
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """
-         filter values in incoming log records using filter_datum
-        """
+        """ filters values in incoming log records """
         return filter_datum(
             self.fields, self.REDACTION, super(
                 RedactingFormatter, self).format(record),
