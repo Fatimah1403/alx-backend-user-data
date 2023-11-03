916e90def347140e5d94926136e1253bb1d00660
Fatimah1403 <fattyhassan14@gmail.com>
2023-11-03 01:10:39 +0000

    Password Encrpt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,6 +7,5 @@ from bcrypt import hashpw
 
 def hash_password(password):
     """returns a salted, hashed password"""
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
