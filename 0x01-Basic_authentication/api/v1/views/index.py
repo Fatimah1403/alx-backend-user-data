9d0ab415a75d20521e35084066077da8d0f3288c
Fatimah1403 <fattyhassan14@gmail.com>
2023-11-07 20:52:34 +0000

    Unauthorized error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,22 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+# ==============TASK1====================================
+# For testing this new error handler, add a new
+# endpoint in api/v1/views/index.py:
+# Route: GET /api/v1/unauthorized
+# This endpoint must raise a 401 error by using abort - Custom Error Pages
+# By calling abort(401), the error handler for 401 will be executed.
+# In the first terminal:
+# ==============================================================
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """Raise a 401 error"""
+    abort(401)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
