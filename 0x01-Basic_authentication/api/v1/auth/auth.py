f26f0369a0c461fb0933001582874dc3376b0fbd
Fatimah1403 <fattyhassan14@gmail.com>
2023-11-08 00:55:06 +0000

    ?new updates

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,38 +1,34 @@
 #!/usr/bin/env python3
 """
-API Authentication
+API authentication module
 """
 
 from flask import request
-from os import getenv
 from typing import List, TypeVar
 
 
 class Auth:
-    """Auth class to manage the API authentication"""
+    """ Authentication """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """To determine whether authentication
-            is required for an API route
-        """
+        """ Checks if API routes require authentication """
         if path is None or not excluded_paths:
             return True
         for i in excluded_paths:
-            if i.endswith('*') and path.startwith(i[:-1]):
+            if i.endswith('*') and path.startswith(i[:-1]):
                 return False
             elif i in {path, path + '/'}:
                 return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """
-        To check whether Authorization request header is present
-        """
+        """ Checks if Authorization request header is present
+        & contains values """
         if request is None or "Authorization" not in request.headers:
             return None
         else:
-            req_header = request.headers.get('Authorization')
-            return req_header
+            return request.headers.get('Authorization')
 
-    def def current_user(self, request=None) -> TypeVar('User'):
-        """Get the current user from the request """
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ placeholder """
         return None
