a67b0fb7df6e39051cfc659964d187c76fa0840b
Fatimah1403 <fattyhassan14@gmail.com>
2023-11-08 02:32:36 +0000

    Store user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -64,10 +64,12 @@ class BasicAuth(Auth):
             the method is_valid_password of User
             Otherwise, return the User instance
         """
-        if user_email is None or not isinstance(user_email, str):
-            return None
-        if user_pwd is None or not isinstance(user_pwd, str):
-            return None
+        def user_object_from_credentials(
+                self, user_email: str, user_pwd: str) -> TypeVar('User'):
+            if user_email is None or not isinstance(user_email, str):
+                return None
+            if user_pwd is None or not isinstance(user_pwd, str):
+                return None
         try:
             users = User.search({'email': user_email})
         except Exception:
