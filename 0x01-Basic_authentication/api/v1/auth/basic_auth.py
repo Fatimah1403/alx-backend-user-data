fd5838fa6696820d7e1546184cc4150204b7ba52
Fatimah1403 <fattyhassan14@gmail.com>
2023-11-08 01:35:41 +0000

    Updates

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Class BasicAuth"""
 from api.v1.auth.auth import Auth
+from typing import TypeVar
+import base64
 
 
 class BasicAuth(Auth):
     """ Basic Authentication inheriting from Auth """
+    pass
 
