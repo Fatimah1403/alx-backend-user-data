d34c921ee6cae3782a0ee664fb8ab842d429ab77
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-14 23:06:43 -0500

    register_user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """Authentication
 """
 from bcrypt import hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import User
 from db import DB
 
@@ -16,3 +18,27 @@ def _hash_password(password: str) -> bytes:
         bytes: _description_
     """
     return hashpw(password.encode("utf-8"), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """_summary_
+
+        Args:
+            email (str): _description_
+            password (str): _description_
+
+        Returns:
+            User: _description_
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
