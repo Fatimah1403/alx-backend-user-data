5831f140ea4e586a8a8eb8f6864fa8d63798e266
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-14 21:13:08 -0500

    find user by id method added

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -36,5 +38,18 @@ class DB:
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
-        self._session.commit()
+        self._session.commit(self, )
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        method takes in arbitrary keyword arguments and returns
+        the first row found in the users table as filtered
+        by the method’s input arguments.
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        user_table = self._session.query(User).filter_by(**kwargs).first()
+        if not user_table:
+            raise NoResultFound
+        return user_table
