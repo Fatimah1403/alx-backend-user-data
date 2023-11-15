a96343eb4db2a66c3ff7e4d123dd91232b6e3c17
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-14 21:58:20 -0500

    update_user method added

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,7 +49,26 @@ class DB:
         """
         if not kwargs:
             raise InvalidRequestError
-        user_table = self._session.query(User).filter_by(**kwargs).first()
-        if not user_table:
+        user_data = self._session.query(User).filter_by(**kwargs).first()
+        if not user_data:
             raise NoResultFound
-        return user_table
+        return user_data
+    
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """_summary_
+
+        Args:
+            user_id (_type_): method will use find_user_by
+            to locate the user to update
+        """
+        user_rec = self.find_user_by(id=user_id)
+        
+        for key, value in kwargs.items():
+            if hasattr(user_rec, key):
+                setattr(user_rec, key, value)
+            else:
+                raise ValueError
+            
+        self._session.commit()
+        return None
+        
