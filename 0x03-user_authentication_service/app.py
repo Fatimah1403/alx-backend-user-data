6e9536274972a388edf541b66ca40c0c416a7c49
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-16 10:35:12 -0500

    user profile added

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -91,7 +91,7 @@ def profile():
     user = AUTH.get_user_from_session_id(session_cookies)
     if session_cookies is None or user is None:
         abort(403)
-    return jsonify({"email", user.email})
+    return jsonify({"email", user.email}), 200
 
 
 if __name__ == "__main__":
